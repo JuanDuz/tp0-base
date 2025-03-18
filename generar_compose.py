4f5a3bee812a64b6dca710310d6c5c0301705a86
Juan Duzac <duzacj@gmail.com>
2025-03-18 18:27:47 -0300

    ej 1

---
+++ generar_compose.py
@@ -0,0 +1,67 @@
+import sys
+import yaml
+
+class IndentedDumper(yaml.Dumper):
+    """Custom YAML dumper to correctly indent lists."""
+    def increase_indent(self, flow=False, indentless=False):
+        return super(IndentedDumper, self).increase_indent(flow, False)
+
+def generar_compose(archivo_salida, cantidad_clientes):
+    docker_compose = {
+        'name': 'tp0',
+        'services': {
+            'server': {
+                'container_name': 'server',
+                'image': 'server:latest',
+                'entrypoint': 'python3 /main.py',
+                'environment': [
+                    'PYTHONUNBUFFERED=1',
+                    'LOGGING_LEVEL=DEBUG'
+                ],
+                'networks': [
+                    'testing_net'
+                ]
+            }
+        },
+        'networks': {
+            'testing_net': {
+                'ipam': {
+                    'driver': 'default',
+                    'config': [
+                        {'subnet': '172.25.125.0/24'}
+                    ]
+                }
+            }
+        }
+    }
+
+    # Generate client services dynamically
+    for i in range(1, cantidad_clientes + 1):
+        docker_compose['services'][f'client{i}'] = {
+            'container_name': f'client{i}',
+            'image': 'client:latest',
+            'entrypoint': '/client',
+            'environment': [
+                f'CLI_ID={i}',
+                'CLI_LOG_LEVEL=DEBUG'
+            ],
+            'networks': [
+                'testing_net'
+            ],
+            'depends_on': [
+                'server'
+            ]
+        }
+
+    # Save the generated docker-compose YAML file with correct indentation
+    with open(archivo_salida, 'w') as archivo:
+        yaml.dump(docker_compose, archivo, Dumper=IndentedDumper, default_flow_style=False, sort_keys=False, indent=2)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python3 mi-generador.py <output_file> <num_clients>")
+        sys.exit(1)
+
+    archivo_salida = sys.argv[1]
+    cantidad_clientes = int(sys.argv[2])
+    generar_compose(archivo_salida, cantidad_clientes)
