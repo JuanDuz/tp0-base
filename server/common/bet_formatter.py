bb14106ca393cddab0dde5273786e0333b52916d
Juan Duzac <duzacj@gmail.com>
2025-03-25 12:49:05 -0300

    log for debug

--- server/common/bet_formatter.py
+++ server/common/bet_formatter.py
@@ -2,12 +2,13 @@ from common.utils import Bet
 
 
 def format_bet_message(bet):
-    return f"{bet.first_name}|{bet.last_name}|{bet.document_number}|{bet.dob}|{bet.number}"
+    return f"{bet.first_name}|{bet.last_name}|{bet.document_number}|{bet.dob}|{bet.number}|{bet.agency}"
 
 
 def parse_bet_message(message):
     fields = message.split("|")
     if len(fields) != 6:
+        print(message)
         raise ValueError("Invalid bet message format")
     return Bet(
         agency=fields[5],
