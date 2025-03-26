d6d017bcf8b9ff736168e5d71080a64a9ca05436
Juan Duzac <duzacj@gmail.com>
2025-03-26 13:13:37 -0300

    refactor client loop

--- client/common/client.go
+++ client/common/client.go
@@ -20,62 +20,112 @@ type ClientConfig struct {
 
 // Client Entity that encapsulates how
 type Client struct {
-	config ClientConfig
+	config      ClientConfig
+	doneSending bool
 }
 
 // NewClient Initializes a new client receiving the configuration
 // as a parameter
 func NewClient(config ClientConfig) *Client {
 	client := &Client{
-		config: config,
+		config:      config,
+		doneSending: false,
 	}
 	return client
 }
 
 // StartClientLoop Send messages to the client until some time threshold is met
 func (c *Client) StartClientLoop(ctx context.Context) {
-	filePath := "/data/agency-" + c.config.ID + ".csv"
-	loader, err := NewBetLoader(filePath)
+	loader, err := NewBetLoader("/data/agency-" + c.config.ID + ".csv")
 	if err != nil {
-		log.Criticalf("action: open_file | result: fail | file: %s | error: %v", filePath, err)
+		log.Criticalf("action: open_file | result: fail | error: %v", err)
 		return
 	}
-	defer func() {
-		if err := loader.Close(); err != nil {
-			log.Errorf("action: close_csv | result: fail | error: %v", err)
-		} else {
-			log.Infof("action: close_csv | result: success")
-		}
-	}()
+	defer c.closeLoader(loader)
 
 	for {
 		select {
 		case <-ctx.Done():
-			log.Infof("action: shutdown | result: in_progress | reason: received SIGTERM | SIGINT")
-			log.Infof("action: shutdown | result: success")
+			c.shutdown()
 			return
 		default:
 		}
-		batch, err := loader.NextBatch(c.config.BatchSize, c.config.ID)
-		if err != nil {
-			if err.Error() == "EOF" {
+		if !c.doneSending {
+			c.sendNextBatch(loader)
+		} else {
+			if c.tryGetWinners() {
 				return
 			}
-			log.Errorf("action: read_batch | result: fail | error: %v", err)
-			return
 		}
 
-		betClient, err := NewBetClient(c.config)
-		if err != nil {
-			log.Errorf("action: connect | result: fail | error: %v", err)
-			continue
-		}
-
-		err = betClient.SendBetBatch(batch)
-		betClient.Close()
-		if err != nil {
-			continue
-		}
 		time.Sleep(c.config.LoopPeriod)
 	}
+}
+
+func (c *Client) closeLoader(loader *BetLoader) {
+	if err := loader.Close(); err != nil {
+		log.Errorf("action: close_csv | result: fail | error: %v", err)
+	} else {
+		log.Infof("action: close_csv | result: success")
+	}
+}
+
+func (c *Client) shutdown() {
+	log.Infof("action: shutdown | result: in_progress | reason: received SIGTERM | SIGINT")
+	log.Infof("action: shutdown | result: success")
+}
+
+func (c *Client) sendNextBatch(loader *BetLoader) {
+	batch, err := loader.NextBatch(c.config.BatchSize, c.config.ID)
+	if err != nil {
+		if err.Error() == "EOF" {
+			c.doneSending = true
+			return
+		}
+		log.Errorf("action: read_batch | result: fail | error: %v", err)
+		return
+	}
+
+	err = c.withBetClient(func(betClient *BetClient) error {
+		return betClient.SendBetBatch(batch)
+	})
+	if err != nil {
+		return
+	}
+}
+
+func (c *Client) tryGetWinners() bool {
+	var retry = true
+	err := c.withBetClient(func(betClient *BetClient) error {
+		winners, err := betClient.GetWinners(c.config.ID)
+		if err != nil {
+			switch err.Error() {
+			case "ERROR_LOTTERY_HASNT_ENDED":
+				log.Infof("action: consulta_ganadores | result: pending")
+				return nil // retry later
+			default:
+				log.Errorf("action: consulta_ganadores | result: fail | reason: %v", err)
+				return err
+			}
+		}
+
+		log.Infof("action: consulta_ganadores | result: success | cant_ganadores: %d", len(winners))
+		retry = false
+		return nil
+	})
+	if err != nil {
+		return false
+	}
+	return retry
+}
+
+func (c *Client) withBetClient(callback func(*BetClient) error) error {
+	betClient, err := NewBetClient(c.config)
+	if err != nil {
+		log.Errorf("action: connect | result: fail | error: %v", err)
+		return err
+	}
+	defer betClient.Close()
+
+	return callback(betClient)
 }
